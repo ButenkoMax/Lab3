d1beaaa3c9f907973c32171d41250fc0e014be22
ButenkoMax <145120576+ButenkoMax@users.noreply.github.com>
2023-11-02 11:18:23 +0300

    Add files via upload

---
+++ masivp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+  
+    int numbers[3];
+    for(int i=1; i <=3; i++)
+    {
+       numbers[i] = i+4;
+       cout << numbers[i] << endl;
+    }
+}
